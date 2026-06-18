932b44b04998cc0e317f4d3712c323cae848c329
AceFVA <acefrancisagustin@gmail.com>
2026-06-18 10:59:44 +0800

    Created a description for this program

---
+++ cpu_scheduling_algo.py
@@ -0,0 +1,11 @@
+# Operating Systems: CPU Scheduling Algorithms
+
+# In this program, we will implement various CPU scheduling algorithms in Python. The algorithms we will cover include:
+# 1. First-Come, First-Served (FCFS)
+# 2. Shortest Job Next (SJN) (Preemptive and Non-Preemptive)
+# 3. Priority Scheduling (Preemptive and Non-Preemptive)
+# 4. Round Robin (RR)
+
+# This program will allow users to input the number of processes, their burst times, arrival times, and priorities (if applicable).
+# This will then calculate and display the average turnararound time and average waiting time for the selected scheduling algorithm 
+# with the use of Tkinter for visualization of the Ghantt chart as well.
